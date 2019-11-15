ac3bbf112190a74b5ea24b63a8ebac58f4fe7447
sdfsdf <sdfjksdl@sdklfj.com>
2019-11-15 01:06:17 -0800

    added ex2.c file

---
+++ ex2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main()
+{
+unsigned int a,b,c;
+unsigned int a = 20 20 +;
+unsigned int b = 1 3 /;
+unsigned int c = 50 0 /;
+a = getchar();
+printf("The expression has a value of:=%d\n",20+20);
+b = getchar();
+printf("The expression has a value of:=%d\n",1/3);
+c = getchar();
+printf("Error:cannot divide a number by 0!%d\n")
+}
